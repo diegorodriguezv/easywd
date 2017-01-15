b28528993138d9f11a2f2b42dc54d0fcc5746552
Diego F. Rodríguez V <diegorodriguezv@gmail.com>
2017-01-15 15:08:43 -0500

    Fix parameter handling

--- index.py
+++ index.py
@@ -11,23 +11,20 @@ cgitb.enable()
 print "Content-Type: text/plain;charset=utf-8"
 print
 
-# cgi.test()
-# print "Hello World!"
-# defaults = {lang:"es", size:20, sep:"-"}
 lang = "es"
 size = 20
 sep = "-"
 args = cgi.FieldStorage()
 if "lang" in args:
-    if args["lang"][0] in ["en", "es"]:
-        lang = args["lang"][0]
-    if "size" in args:
-        try:
-            if int(args["size"][0]) in range(4, 50 + 1):
-                size = int(args["size"][0])
-        except ValueError:
-            pass
-    if "sep" in args:
-        sep = args["sep"][0]
+    if args["lang"] in ["en", "es"]:
+        lang = args["lang"]
+if "size" in args:
+    try:
+        if int(args["size"]) in range(4, 50 + 1):
+            size = int(args["size"])
+    except ValueError:
+        pass
+if "sep" in args:
+    sep = args["sep"]
 html = easywd.make_password(lang, size, sep)
 print html
